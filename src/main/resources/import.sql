2b6eb6dddbfa1f8becdcdc7e2535d2da5cf3d56d
Fernando Tito <fernando.tito.contractor@bbva.com>
2023-05-21 11:59:46 -0500

    Actualizando configuracion y collection de postman

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuario(id_usuario,nombre_usuario,rol_usuario,password,username) VALUES (1,'Fernando Tito',NULL,'$2a$10$XCk9EpBaRxNTCeXmuWAh/OMs74Orm6d/iOxzvhe/lUwRd5Y4NbJfW','ftito');
+INSERT INTO usuario(id_usuario,nombre_usuario,rol_usuario,password,username) values (2,'Bruno Diaz',NULL,'$2a$10$XCk9EpBaRxNTCeXmuWAh/OMs74Orm6d/iOxzvhe/lUwRd5Y4NbJfW','bdiaz');
+COMMIT;
